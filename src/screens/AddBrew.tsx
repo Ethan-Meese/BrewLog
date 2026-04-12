2482dd2a9a08d5257f756fff4d3a17e81b9a3408
Ethan Meese <meeseethan@gmail.com>
2026-04-12 14:51:12 -0400

    Added a lot of new features including automatic ratio calculation, espresso options and other drink options iput fields auto updating, a temp scroll feature in the brew logs screen.

--- src/screens/AddBrew.tsx
+++ src/screens/AddBrew.tsx
@@ -3,10 +3,14 @@ import { View, Text, StyleSheet, Button, TextInput} from 'react-native';
 import { Picker } from '@react-native-picker/picker'; // dropdown
 import {brewLogs} from './BrewLogs';
 export interface Brew {
+  _brewMethod:string;
   _coffeeName:string;
   _coffeeType:string;
   _coffeeDose: string;
-  _brewMethod:string;
+  _water: string;
+  _ratio: string | null;
+  _waterTemp?: string;
+  _time?: string;
   _grinder:string;
   _grindSize:string;
   _rating:string;
@@ -25,7 +29,7 @@ function BrewInput({label, value, onChange, placeholder, keyboardType = "default
   return(
     <View>
       <Text>{label}</Text>
-      <TextInput 
+      <TextInput
       value={value}
       onChangeText={onChange}
       placeholder={placeholder}
@@ -38,22 +42,38 @@ function BrewInput({label, value, onChange, placeholder, keyboardType = "default
 
 export default function AddBrew({navigation}: any) {
 
+  const [brewMethod, setBrewMethod] = useState("Espresso");
   const [coffeeName, setCoffeeName] = useState("")
   const [coffeeType, setCoffeeType] = useState("");
   const [coffeeDose, setCoffeeDose] = useState("");
-  const [brewMethod, setBrewMethod] = useState("Espresso");
+  const [water, setWater] = useState("");
+  // Ratio is made down below
+  const [waterTemp, setWaterTemp] = useState("");
+  const [time, setTime] = useState("");
   const [grinder, setGrinder] = useState("");
   const [grindSize, setGrindSize] = useState("");
   const [rating, setRating] = useState("");
   const [notes, setNotes] = useState("");
 
+
+  // Making the ratio calculation and converting it to work with the Text tag
+  const doseNumber: number = parseFloat(coffeeDose);
+  const waterNumber: number = parseFloat(water);
+  const ratio: string | null = !isNaN(doseNumber) && doseNumber > 0 && !isNaN(waterNumber) ? (waterNumber / doseNumber).toFixed(1) : null;
+  console.log(ratio);
+
+
   const handleSave = () => {
     // store brew locally later
     var newBrew: Brew = {
+      _brewMethod: brewMethod, 
       _coffeeName: coffeeName, 
       _coffeeType: coffeeType,
       _coffeeDose: coffeeDose,
-      _brewMethod: brewMethod, 
+      _water: water,
+      _ratio: ratio,
+      _waterTemp: waterTemp,
+      _time: time,
       _grinder: grinder, 
       _grindSize: grindSize, 
       _rating: rating, 
@@ -86,19 +106,30 @@ export default function AddBrew({navigation}: any) {
           <Picker.Item label="Cold Brew" value="Cold Brew"/>
         </Picker>
 
-      {brewMethod === "Espresso" &&(
-        <Text>Testing for Espresso</Text>
-      )}
-
-      {brewMethod === "Pour-Over" &&(
-        <Text>Testing for Pour-Over</Text>
-      )}
-      
       <BrewInput label='Coffee Name' value={coffeeName} onChange={setCoffeeName} placeholder='Enter coffee name...'/>
 
-      <BrewInput label='Coffee Origin' value={coffeeType} onChange={setCoffeeType} placeholder='Enter coffee Origin...'/>
+      <BrewInput label='Coffee Origin' value={coffeeType} onChange={setCoffeeType} placeholder='Enter coffee origin...'/>
 
-      <BrewInput label='Coffee Dose' value={coffeeDose} onChange={setCoffeeDose} placeholder='Enter coffee Dose...'/>
+      <BrewInput label='Coffee Dose' value={coffeeDose} onChange={setCoffeeDose} placeholder='Enter coffee dose...'/>
+
+
+      {brewMethod !== "Espresso" &&(
+        <>
+          <BrewInput label='Water Amount' value={water} onChange={setWater} placeholder='Enter water amount...' />
+          <Text>{ratio !== null ? `Ratio is 1:${ratio}` : "Enter dose and water to get the ratio"}</Text>
+        </>
+      )}
+      
+      {brewMethod === "Espresso" &&(
+        <>
+        <BrewInput label='Espresso Yield' value={water} onChange={setWater} placeholder='Espresso yield...' />
+        <Text>{ratio !== null ? `Ratio is 1:${ratio}` : "Enter dose and yield to get the ratio"}</Text>
+        </>
+      )}
+
+      <BrewInput label='Water Tempature' value={waterTemp} onChange={setWaterTemp} placeholder='Enter water tempature...'/>
+
+      <BrewInput label='Brew Time' value={time} onChange={setTime} placeholder='Enter brew time...' />
 
       <BrewInput label='Grinder' value={grinder} onChange={setGrinder} placeholder='Enter grinder...'/>
 
--- src/screens/BrewLogs.tsx
+++ src/screens/BrewLogs.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Brew } from './AddBrew';
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
 
 export let brewLogs:Brew[] = [];
 
 export default function BrewLogs() {
   return (
-    <View style={styles.container}>
-      <Text>Brew Log Screen!</Text>
-      <Text>You have {brewLogs.length} brew logs!</Text>
-      <Text></Text>
+    <GestureHandlerRootView style={styles.container}>
+      <ScrollView>
+        <Text>Brew Log Screen!</Text>
+        <Text>You have {brewLogs.length} brew logs!</Text>
+        <Text></Text>
 
-        {brewLogs.map((brew, index) => (
-          <View key={index}>
-            <Text>{brew._brewMethod}</Text>
-            <Text>{brew._coffeeName}</Text> 
-            <Text>{brew._coffeeType} </Text>
-            <Text>{brew._grinder}</Text>
-            <Text>{brew._grindSize}</Text>
-            <Text>{brew._rating} </Text>
-            <Text>{brew._notes}</Text>
-            <Text></Text>
-          </View>
-        ))}
-    </View>
+          {brewLogs.map((brew, index) => (
+            <View key={index}>
+              <Text>Brew Method: {brew._brewMethod}</Text>
+              <Text>Coffee Name: {brew._coffeeName}</Text> 
+              <Text>Coffee Type: {brew._coffeeType} </Text>
+              <Text>Coffee Dose: {brew._coffeeDose}</Text>
+              <Text>Water Dose: {brew._water}</Text>
+              <Text>Ratio: {brew._ratio !== null ? `1:${brew._ratio}` : "No Ratio Given"}</Text>
+              <Text>Water Tempature: {brew._waterTemp}</Text>
+              <Text>Brew Time: {brew._time}</Text>
+              <Text>Grinder: {brew._grinder}</Text>
+              <Text>Grind Size: {brew._grindSize}</Text>
+              <Text>Rating: {brew._rating} </Text>
+              <Text>Notes: {brew._notes}</Text>
+              <Text></Text>
+            </View>
+          ))}
+      </ScrollView>
+    </GestureHandlerRootView>
+    
   );
 }
 
