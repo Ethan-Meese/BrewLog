febc477d6ef6e0566315bff1a75e65ff0ac2d1b8
Ethan Meese <meeseethan@gmail.com>
2026-04-23 21:28:57 -0400

    Added the feature to remove grinders from inside the grinders screen. This updates across the whole app. I have to add this to the brewlogs screen as well as the coffees screen. I may add a feature to add your own brew methods and the ability to remove brew methods.

--- src/screens/Grinders.tsx
+++ src/screens/Grinders.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
 import { Grinder } from './AddGrinder';
 
@@ -7,20 +7,53 @@ export let grinders:Grinder[] = [];
 
 
 export default function Ginders({navigation}: any) {
+
+  const handleGrindersUpdate = () =>{
+    grinders = grinderList;
+    console.log(grinders);
+    navigation.goBack();
+  }
+
+  const [isEditing, setEditing] = useState(false);
+  const [grinderList, setGrinderList] = useState<Grinder[]>(grinders);
+
+  const removeGrinder = (index: number) =>{
+    const tempGrindersArr = [...grinderList];
+    tempGrindersArr.splice(index,1);
+    setGrinderList(tempGrindersArr);
+  };
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <ScrollView>
         <Text>Grinders Screen!</Text>
-        <Text>You have {grinders.length} grinders!</Text>
+        <Text>You have {grinderList.length} grinders!</Text>
+        {(grinderList.length > 0 || isEditing === true)&&(
+          <>
+            <Button title={isEditing ? "Done" : "Edit Grinders"} onPress={() => setEditing(!isEditing)} />
+            <Button title="Check edit state" onPress={() => console.log(isEditing)} />
+          </>
+        )}
+        
         <Text></Text>
 
-          {grinders.map((grinder, index) => (
+          {grinderList.map((grinder, index) => (
             <View key={index}>
               <Text>Grinder Name: {grinder._grinderName}</Text>
               <Text>Notes: {grinder._notes !== "" ? grinder._notes : "N/A"}</Text>
+              {isEditing && (
+                <>
+                <Button title="Remove Grinder" onPress={ () => removeGrinder(index)}/>
+                </>
+              )}
               <Text></Text>
             </View>
           ))}
+
+          {grinderList.length > 0 && (
+            <Button title="Save" onPress={handleGrindersUpdate}/>
+          )}
+
       </ScrollView>
     </GestureHandlerRootView>
     
